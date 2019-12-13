fc165baf15ab83f14e6c72beb63bdf998467bb8a
Wyatt Bucki <768692@apps.district196.org>
2019-12-13 07:47:28 -0600

    Initial Commit

---
+++ Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test
+//
+//  Created by BUCKI, WYATT on 12/13/19.
+//  Copyright © 2019 district196.org. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
